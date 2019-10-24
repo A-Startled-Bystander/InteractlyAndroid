46ab21227e4ea71f85b7e09bf50dc44d95080712
Souldah <aylott1@hotmail.com>
2019-10-24 23:48:53 +0200

    Added Menu navigation to homepage... Header needs work to make it look better

--- app/src/main/java/com/example/interactly/Home.java
+++ app/src/main/java/com/example/interactly/Home.java
@@ -1,13 +1,77 @@
 package com.example.interactly;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity
+implements NavigationView.OnNavigationItemSelectedListener {
+    //MENU
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        //MENU
+        mDrawerLayout = findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(Home.this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(Home.this);
+        navigationView.bringToFront();
+        //
+
+
+
     }
+
+
+    //------------MENU
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item))
+        {return true;}
+        return super.onOptionsItemSelected(item);
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+
+        switch (item.getItemId()){
+
+            case R.id.home:
+                intent = new Intent(Home.this, Home.class);
+                startActivity(intent);
+                break;
+            case R.id.profile:
+                Toast.makeText(this, "profile test", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                intent = new Intent(Home.this, Login.class);
+                startActivity(intent);
+                break;
+            default:
+                intent = new Intent(Home.this, Home.class);
+                startActivity(intent);
+                break;
+
+        }
+
+        return true;
+    }
+    //-----------MENU END
+
 }
