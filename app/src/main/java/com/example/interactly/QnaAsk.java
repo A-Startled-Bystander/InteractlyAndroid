1c834d0d3329c544951820a7e72291b6f28195d5
Souldah <aylott1@hotmail.com>
2019-11-17 14:08:36 +0200

    Login EventCode Go button

---
+++ app/src/main/java/com/example/interactly/QnaAsk.java
@@ -0,0 +1,13 @@
+package com.example.interactly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QnaAsk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qna_ask);
+    }
+}
